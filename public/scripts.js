d293accfacc7c00a2296f2392459c7357fe2569f
Wesley henrique <wesley.henrique.pgm@gmail.com>
2020-01-25 15:01:14 -0300

    validação de email no front

--- public/scripts.js
+++ public/scripts.js
@@ -1,16 +1,49 @@
 const Mask = {
     apply(input, func) {
-        setTimeout(function() {
+        setTimeout(function () {
             input.value = Mask[func](input.value)
         }, 1)
     },
     formatBRL(value) {
-        value = value.replace(/\D/g,"")
+        value = value.replace(/\D/g, "")
 
         return new Intl.NumberFormat('pt-BR', {
             style: 'currency',
             currency: 'BRL'
-        }).format(value/100)
+        }).format(value / 100)
+    },
+    cpfCnpj(value) {
+        value = value.replace(/\D/g, "")
+
+        if (value.length > 14) value = value.slice(0, -1)
+
+        //check if is cpf or cnpj
+        //mascara CNPJ
+        if (value.length > 11) {
+            value = value.replace(/(\d{2})(\d)/, "$1.$2")
+            value = value.replace(/(\d{3})(\d)/, "$1.$2")
+            value = value.replace(/(\d{3})(\d)/, "$1/$2")
+            value = value.replace(/(\d{4})(\d)/, "$1-$2")
+
+        } else {
+
+        }
+        // mascara cpf
+        value = value.replace(/(\d{3})(\d)/, "$1.$2")
+        value = value.replace(/(\d{3})(\d)/, "$1.$2")
+        value = value.replace(/(\d{3})(\d)/, "$1-$2")
+
+        return value
+    },
+    cep(value) {
+        value = value.replace(/\D/g, "")
+
+        if (value.length > 8) value = value.slice(0, -1)
+
+
+        value = value.replace(/(\d{5})(\d)/, "$1-$2")
+
+        return value
     }
 }
 
@@ -20,11 +53,11 @@ const PhotosUpload = {
     uploadLimit: 6,
     files: [],
     handleFileInput(event) {
-        
+
         const { files: fileList } = event.target
         PhotosUpload.input = event.target
 
-        if(PhotosUpload.hasLimit(event)) return
+        if (PhotosUpload.hasLimit(event)) return
 
 
         Array.from(fileList).forEach(file => {
@@ -50,10 +83,10 @@ const PhotosUpload = {
     },
 
     hasLimit(event) {
-            //limite de fotos
-        const { uploadLimit, input, preview} = PhotosUpload
+        //limite de fotos
+        const { uploadLimit, input, preview } = PhotosUpload
         const { files: fileList } = input
-             
+
         if (fileList.length > uploadLimit) {
             alert(`Envie no máximo ${uploadLimit} fotos`)
             event.preventDefault()
@@ -63,22 +96,22 @@ const PhotosUpload = {
         const photosDiv = []
         preview.childNodes.forEach(item => {
             if (item.classList && item.classList.value == "photo")
-            photosDiv.push(item)
+                photosDiv.push(item)
         })
 
         const totalPhotos = fileList.length + photosDiv.length
 
-        if (totalPhotos > uploadLimit){
+        if (totalPhotos > uploadLimit) {
             alert("Você atingiu o limite máximo de fotos")
             event.preventDefault()
             return true
         }
 
         return false
-        
+
     },
 
-    getAllFiles(){
+    getAllFiles() {
         const dataTransfer = new ClipboardEvent("").clipboardData || new DataTransfer()
 
         PhotosUpload.files.forEach(file => dataTransfer.items.add(file))
@@ -89,10 +122,10 @@ const PhotosUpload = {
 
     getContainer(image) {
         const div = document.createElement('div')
-        
+
         div.classList.add('photo')
 
-        div.onclick =  PhotosUpload.removePhoto
+        div.onclick = PhotosUpload.removePhoto
 
         div.appendChild(image)
 
@@ -106,7 +139,7 @@ const PhotosUpload = {
         button.innerHTML = "close"
         return button
     },
-    removePhoto(event){
+    removePhoto(event) {
         const photoDiv = event.target.parentNode // <i> div class"photo"
         const photosArray = Array.from(PhotosUpload.preview.children)
         const index = photosArray.indexOf(photoDiv)
@@ -120,7 +153,7 @@ const PhotosUpload = {
     removeOldPhoto(event) { //remove foto no back
         const photoDiv = event.target.parentNode
 
-        if(photoDiv.id) {
+        if (photoDiv.id) {
             const removedFiles = document.querySelector('input[name="removed_files"')
             if (removedFiles) {
                 removedFiles.value += `${photoDiv.id},` //
@@ -134,12 +167,12 @@ const PhotosUpload = {
 const ImageGallery = { //a imagem fica na forma ativada ou inativa
     highlight: document.querySelector('.gallery .highlight > img'), //alterar a foto principal
     previews: document.querySelectorAll('.gallery-preview img'), //pega as imagens
-    setImage(e){
+    setImage(e) {
         const { target } = e
 
-        ImageGallery.previews.forEach( preview => preview.classList.remove('active')) //remova o active
+        ImageGallery.previews.forEach(preview => preview.classList.remove('active')) //remova o active
         target.classList.add('active')
-        
+
         ImageGallery.highlight.src = target.src
         LightBox.image.src = target.src
     }
@@ -150,18 +183,61 @@ const LightBox = {
     image: document.querySelector('.lightbox-target img'),
     closeButton: document.querySelector('lightbox-target a.lightbox-close'),
 
-    open(){
+    open() {
         LightBox.target.style.opacity = 1
         LightBox.target.style.top = 0
         LightBox.target.style.bottom = 0
-        LightBox.closeButton.style.top = 0 
+        LightBox.closeButton.style.top = 0
     },
 
-    close(){
+    close() {
 
         LightBox.target.style.opacity = 0
         LightBox.target.style.top = "-100%"
         LightBox.target.style.bottom = "initial"
         LightBox.closeButton.style.top = "-80px"
+    }
+}
+
+const Validate = {
+    apply(input, func) {
+        Validate.clearErrors(input)
+
+
+
+        let results = Validate[func](input.value)
+        input.value = results.value
+
+        if (results.error)
+            Validate.displayError(input, results.error)
+    },
+    displayError(input, error) {
+        const div = document.createElement('div')
+        div.classList.add('error')
+        div.innerHTML = error
+        input.parentNode.appendChild(div)
+
+        input.focus() //nao deixa sair do campo sem o formulario
+    },
+
+    clearErrors(input){
+        const errorDiv = input.parentNode.querySelector(".error")
+
+        if (errorDiv){
+            errorDiv.remove()
+        }
+    },
+
+    isEmail(value) {
+        let error = null
+
+        //validacao email
+        const mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+
+        if (!value.match(mailFormat)) error = "*Email inválido*"
+        return {
+            error,
+            value
+        }
     }
 }
