21c0db21120b40a4eee3aba8bcbd7528de05c607
Wesley henrique <wesley.henrique.pgm@gmail.com>
2020-02-08 13:18:31 -0300

    recuperadação de senha

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,5 +1,6 @@
 const User = require('../models/User')
 
+const { hash } = require('bcryptjs')
 const crypto = require('crypto')
 const mailer = require('../../lib/mailer')
 
@@ -73,22 +74,34 @@ module.exports = {
     resetForm(req, res) {
         return res.render("session/password-reset", { token: req.query.token })
     },
-    reset(req, res) {
-        const { email, password, passwordRepeat, token } = req.body
+    async reset(req, res) {
+        const  user  = req.user
+        const {  password, token } = req.body
 
         try {
             //cria um novo hash de senha
+            const newPassword = await hash(password, 8)
 
             //atualiza o usuario
+            await User.update(user.id,{
+                password: newPassword,
+                reset_token:"",
+                reset_token_expires:""
+            })
 
             //avisa o usuario que ele tem uma nova senha
+            return res.render("session/login", {
+                user:req.body,
+                success:"Senha atualizada!Faça seu login" 
+            })
 
         } catch (err) {
             console.error(err)
                 ("session/password-reset", {
-                    success: "Erro inesperado"
+                    user:req.body,
+                    token,
+                    error: "Erro inesperado"
                 })
-
         }
     }
 
